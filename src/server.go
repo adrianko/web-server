f300651f8aafd1327bf1d01be04a5db274142820
Adrian Ko <adrianchko@gmail.com>
2015-06-29 08:00:47 +0100

    Add basic listen and serve

--- src/server.go
+++ src/server.go
@@ -2,8 +2,28 @@ package main
 
 import (
     "fmt"
+    "net/http"
+    "io"
 )
 
+var mux map[string]func(http.ResponseWriter, *http.Request)
+
+type Handler struct {}
+
+func (*Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    if h, ok := mux[r.URL.String()]; ok {
+        h(w, r)
+        return
+    }
+    
+    io.WriteString(w, "Server: " + r.URL.String())
+}
+
 func main() {
-    fmt.Println("Hello, world")
+    fmt.Println("Running server 127.0.0.1:8000")
+    server := http.Server{Addr: ":8000", Handler: &Handler{}}
+    
+    mux = make(map[string]func(http.ResponseWriter, *http.Request))
+    mux["/"] = nil
+    server.ListenAndServe()
 }
