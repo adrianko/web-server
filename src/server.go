a8e4a739b10540e25a23198a049b00293d0a6295
Adrian Ko <adrianchko@gmail.com>
2015-06-29 09:33:05 +0100

    Rename handlers map

--- src/server.go
+++ src/server.go
@@ -6,25 +6,24 @@ import (
     "io"
 )
 
-var mux map[string]func(http.ResponseWriter, *http.Request) = make(map[string]func(http.ResponseWriter, *http.Request))
+var handlers map[string]func(http.ResponseWriter, *http.Request) = make(map[string]func(http.ResponseWriter, 
+    *http.Request))
 
 type Handler struct {}
 
 func (*Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    if handle, ok := mux[r.URL.String()]; ok {
+    if handle, ok := handlers[r.URL.String()]; ok {
         handle(w, r)
         return
     }
-    
-    io.WriteString(w, "Server: " + r.URL.String())
 }
 
 func main() {
     fmt.Println("Running server 127.0.0.1:8000")
     server := http.Server{Addr: ":8000", Handler: &Handler{}}
     
-    mux["/"] = hello
-    mux["/html"] = helloHTML
+    handlers["/"] = hello
+    handlers["/html"] = helloHTML
     server.ListenAndServe()
 }
 
