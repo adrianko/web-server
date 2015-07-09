c61e2446c0e71c6f1312f1210b528d93f6bc253c
Adrian Ko <adrianchko@gmail.com>
2015-07-09 14:18:02 +0100

    Remove existing handlers

--- src/server.go
+++ src/server.go
@@ -16,13 +16,16 @@ var handlers map[string]func(http.ResponseWriter, *http.Request) = make(map[stri
 type Handler struct {}
 
 func (*Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    if handle, ok := handlers[r.URL.String()]; ok {
-        handle(w, r)
+    
+}
 
-        return
-    }
-
-    reqError(w, r)
+func main() {
+    data, err := ioutil.ReadFile("../conf/config")
+    check(err)
+    parse_config(string(data))
+    log.Printf("Running server %s:%s\n", configuration["interface"], configuration["port"])
+    server := http.Server{Addr: configuration["interface"] + ":" + configuration["port"], Handler: &Handler{}}
+    server.ListenAndServe()
 }
 
 func check(e error) {
@@ -31,42 +34,25 @@ func check(e error) {
     }
 }
 
-func main() {
-    data, err := ioutil.ReadFile("../conf/config")
-    check(err)
-    parseConfig(string(data))
-    log.Printf("Running server %s:%s\n", configuration["interface"], configuration["port"])
-    server := http.Server{Addr: configuration["interface"] + ":" + configuration["port"], Handler: &Handler{}}
-
-    handlers["/"] = hello
-    handlers["/html"] = helloHTML
-    handlers["/json"] = helloJSON
-    server.ListenAndServe()
-}
-
-func parseConfig(config string) {
+func parse_config(config string) {
 	for _, c := range strings.Split(config, "\n") {
 		line := strings.Split(c, "=")
 		configuration[strings.Trim(line[0], " ")] = strings.Trim(line[1], " ")
 	}
 }
 
-func logRequest(r *http.Request, w http.ResponseWriter, status int) {
+func log_request(r *http.Request, w http.ResponseWriter, status int) {
     log.Printf("%d %s: %s", status, r.Method, r.URL.String())
 }
 
-func sendOK(r *http.Request, w http.ResponseWriter, contentType string, content string) {
-    send(r, w, http.StatusOK, contentType, content)
-}
-
 func send(r *http.Request, w http.ResponseWriter, status int, contentType string, content string) {
-    setContentType(w, contentType)
+    set_content_type(w, contentType)
     w.WriteHeader(status)
-    logRequest(r, w, status)
+    log_request(r, w, status)
     io.WriteString(w, content)
 }
 
-func setContentType(w http.ResponseWriter, contentType string) {
+func set_content_type(w http.ResponseWriter, contentType string) {
     var cType string
 
     switch contentType {
@@ -83,21 +69,4 @@ func setContentType(w http.ResponseWriter, contentType string) {
     }
 
     w.Header().Set("Content-Type", cType)
-}
-
-//handlers
-func hello(w http.ResponseWriter, r *http.Request) {
-    sendOK(r, w, "plain", "Hello world")
-}
-
-func helloHTML(w http.ResponseWriter, r *http.Request) {
-    sendOK(r, w, "html", "<h1>Hello world</h1>")
-}
-
-func helloJSON(w http.ResponseWriter, r *http.Request) {
-    sendOK(r, w, "json", "{\"hello\": \"world\"}")
-}
-
-func reqError(w http.ResponseWriter, r *http.Request) {
-    send(r, w, http.StatusNotFound, "html", "<h1>Not found</h1>")
 }
