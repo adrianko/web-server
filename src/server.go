cd3452d2cdcbd9f43c229d1e2d53f726e8f5087d
Adrian Ko <adrianchko@gmail.com>
2015-07-09 09:55:04 +0100

    Tabs to spaces

--- src/server.go
+++ src/server.go
@@ -1,97 +1,97 @@
 package main
 
 import (
-	"io"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"strconv"
+    "io"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "strconv"
 )
 
 const NIC string = "0.0.0.0"
 const PORT int = 8000
 
 var handlers map[string]func(http.ResponseWriter, *http.Request) = make(map[string]func(http.ResponseWriter,
-	*http.Request))
+    *http.Request))
 
 type Handler struct{}
 
 func (*Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if handle, ok := handlers[r.URL.String()]; ok {
-		handle(w, r)
+    if handle, ok := handlers[r.URL.String()]; ok {
+        handle(w, r)
 
-		return
-	}
+        return
+    }
 
-	reqError(w, r)
+    reqError(w, r)
 }
 
 func check(e error) {
-	if e != nil {
-		panic(e)
-	}
+    if e != nil {
+        panic(e)
+    }
 }
 
 func main() {
-	data, err := ioutil.ReadFile("../conf/config")
-	check(err)
-	log.Print(string(data))
-	log.Printf("Running server %s:%d\n", NIC, PORT)
-	server := http.Server{Addr: NIC + ":" + strconv.Itoa(PORT), Handler: &Handler{}}
+    data, err := ioutil.ReadFile("../conf/config")
+    check(err)
+    log.Print(string(data))
+    log.Printf("Running server %s:%d\n", NIC, PORT)
+    server := http.Server{Addr: NIC + ":" + strconv.Itoa(PORT), Handler: &Handler{}}
 
-	handlers["/"] = hello
-	handlers["/html"] = helloHTML
-	handlers["/json"] = helloJSON
-	server.ListenAndServe()
+    handlers["/"] = hello
+    handlers["/html"] = helloHTML
+    handlers["/json"] = helloJSON
+    server.ListenAndServe()
 }
 
 func logRequest(r *http.Request, w http.ResponseWriter, status int) {
-	log.Printf("%d %s: %s", status, r.Method, r.URL.String())
+    log.Printf("%d %s: %s", status, r.Method, r.URL.String())
 }
 
 func sendOK(r *http.Request, w http.ResponseWriter, contentType string, content string) {
-	send(r, w, http.StatusOK, contentType, content)
+    send(r, w, http.StatusOK, contentType, content)
 }
 
 func send(r *http.Request, w http.ResponseWriter, status int, contentType string, content string) {
-	setContentType(w, contentType)
-	w.WriteHeader(status)
-	logRequest(r, w, status)
-	io.WriteString(w, content)
+    setContentType(w, contentType)
+    w.WriteHeader(status)
+    logRequest(r, w, status)
+    io.WriteString(w, content)
 }
 
 func setContentType(w http.ResponseWriter, contentType string) {
-	var cType string
+    var cType string
 
-	switch contentType {
-	case "json":
-		cType = "application/json"
-		break
-	case "html":
-		cType = "text/html"
-		break
-	case "plain":
-	default:
-		cType = "text/plain"
-		break
-	}
+    switch contentType {
+    case "json":
+        cType = "application/json"
+        break
+    case "html":
+        cType = "text/html"
+        break
+    case "plain":
+    default:
+        cType = "text/plain"
+        break
+    }
 
-	w.Header().Set("Content-Type", cType)
+    w.Header().Set("Content-Type", cType)
 }
 
 //handlers
 func hello(w http.ResponseWriter, r *http.Request) {
-	sendOK(r, w, "plain", "Hello world")
+    sendOK(r, w, "plain", "Hello world")
 }
 
 func helloHTML(w http.ResponseWriter, r *http.Request) {
-	sendOK(r, w, "html", "<h1>Hello world</h1>")
+    sendOK(r, w, "html", "<h1>Hello world</h1>")
 }
 
 func helloJSON(w http.ResponseWriter, r *http.Request) {
-	sendOK(r, w, "json", "{\"hello\": \"world\"}")
+    sendOK(r, w, "json", "{\"hello\": \"world\"}")
 }
 
 func reqError(w http.ResponseWriter, r *http.Request) {
-	send(r, w, http.StatusNotFound, "html", "<h1>Not found</h1>")
+    send(r, w, http.StatusNotFound, "html", "<h1>Not found</h1>")
 }
