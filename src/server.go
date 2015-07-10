7552a605795c7403ee1e433e5c1b20b4bbb4cc33
Adrian Ko <adrianchko@gmail.com>
2015-07-10 10:27:02 +0100

    Check file list

--- src/server.go
+++ src/server.go
@@ -6,6 +6,8 @@ import (
     "log"
     "net/http"
 	"strings"
+    "path/filepath"
+    "fmt"
 )
 
 var configuration map[string]string = make(map[string]string)
@@ -16,7 +18,14 @@ var handlers map[string]func(http.ResponseWriter, *http.Request) = make(map[stri
 type Handler struct {}
 
 func (*Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    if val, ok := configuration["root"]; ok {
+        files, err := filepath.Glob(val)
+        check(err)
+        fmt.Println(files)    
+    }
     
+    w.Header().Set("Content-Type", "")
+    send(r, w, 404, "Hello, world")
 }
 
 func main() {
@@ -45,28 +54,8 @@ func log_request(r *http.Request, w http.ResponseWriter, status int) {
     log.Printf("%d %s: %s", status, r.Method, r.URL.String())
 }
 
-func send(r *http.Request, w http.ResponseWriter, status int, contentType string, content string) {
-    set_content_type(w, contentType)
+func send(r *http.Request, w http.ResponseWriter, status int, content string) {
     w.WriteHeader(status)
     log_request(r, w, status)
     io.WriteString(w, content)
-}
-
-func set_content_type(w http.ResponseWriter, contentType string) {
-    var cType string
-
-    switch contentType {
-    case "json":
-        cType = "application/json"
-        break
-    case "html":
-        cType = "text/html"
-        break
-    case "plain":
-    default:
-        cType = "text/plain"
-        break
-    }
-
-    w.Header().Set("Content-Type", cType)
 }
