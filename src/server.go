270573542f73b4cd8b752957e0ef8588c0946235
Adrian Ko <adrianchko@gmail.com>
2015-07-13 07:40:34 +0100

    Tabs

--- src/server.go
+++ src/server.go
@@ -4,24 +4,24 @@ import (
     "io"
     "io/ioutil"
     "log"
-    "net/http"
-    "strings"
-    "os"
     "mime"
+    "net/http"
+    "os"
+    "strings"
 )
 
 var config_file string = "/etc/maester-http"
 
 var configuration map[string]string = map[string]string{
-    "root": "/var/www",
-    "port": "80",
+    "root":      "/var/www",
+    "port":      "80",
     "interface": "0.0.0.0",
-    "index": "index.html",
+    "index":     "index.html",
 }
 
 var index_files []string = []string{}
 
-type Handler struct {}
+type Handler struct{}
 
 func (*Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     if valid, path := valid_path(configuration["root"] + r.URL.String()); valid {
@@ -42,13 +42,13 @@ func parse_config(config string) {
         if strings.HasPrefix(c, "#") || strings.HasPrefix(c, ";") {
             continue
         }
-        
+
         if strings.Contains(c, "=") {
             line := strings.Split(c, "=")
             configuration[strings.TrimSpace(line[0])] = strings.TrimSpace(line[1])
         }
     }
-    
+
     validate_config()
 }
 
@@ -57,7 +57,7 @@ func validate_config() {
     if _, err := os.Stat(configuration["root"]); os.IsNotExist(err) {
         log.Fatal("Root path does not exist.")
     }
-    
+
     for _, in := range strings.Split(configuration["index"], " ") {
         index_files = append(index_files, strings.TrimSpace(in))
     }
@@ -67,15 +67,14 @@ func valid_path(path string) (bool, string) {
     if valid_file(path) {
         return true, path
     }
-    
+
     if strings.HasSuffix(path, "/") {
         return valid_index(path)
     }
-    
+
     if !strings.HasSuffix(path, "/") && !strings.HasSuffix(path, "index.html") {
         return valid_path(path + "/")
     }
-    
     return false, path
 }
 
@@ -86,10 +85,10 @@ func valid_index(path string) (bool, string) {
                 return true, path + in
             }
         }
-        
+
         return false, path
     }
-        
+
     return valid_index(path + "/")
 }
 
@@ -97,23 +96,23 @@ func valid_file(path string) bool {
     if info, err := os.Stat(path); err == nil && !info.IsDir() {
         return true
     }
-    
+
     return false
 }
 
 func send(r *http.Request, w http.ResponseWriter, static_file string) {
     data, err := ioutil.ReadFile(static_file)
-    
+
     if err != nil {
         log.Printf("Could not read file: " + static_file)
         return
     }
-    
+
     filePath := strings.Split(static_file, "/")
-    
-    if !strings.HasPrefix(filePath[len(filePath) - 1], ".") {
-        fileName := strings.Split(filePath[len(filePath) - 1], ".")
-        ext := "." + fileName[len(fileName) - 1]
+
+    if !strings.HasPrefix(filePath[len(filePath)-1], ".") {
+        fileName := strings.Split(filePath[len(filePath)-1], ".")
+        ext := "." + fileName[len(fileName)-1]
         send_response(r, w, 200, mime.TypeByExtension(ext), string(data))
     } else {
         send_not_found(r, w)
@@ -147,7 +146,7 @@ func main() {
     if len(os.Args) > 1 {
         config_file = os.Args[1]
     }
-    
+
     load_config()
     start_server()
 }
