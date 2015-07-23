c66a63f5760d6cf0af42b49727dec4287d1369b2
Adrian Ko <adrianchko@gmail.com>
2015-07-23 13:33:27 +0100

    Add invalid dir test

--- server_test.go
+++ server_test.go
@@ -4,10 +4,18 @@ import (
     "testing"
 )
 
-func Test_valid_dir(t * testing.T) {
+func Test_valid_dir_valid(t *testing.T) {
     valid := valid_dir("/etc")
 
     if !valid {
-        t.Errorf("valid_dir: /etc")
+        t.Errorf("Valid directory is invalid")
+    }
+}
+
+func Test_valid_dir_invalid(t *testing.T) {
+    valid := valid_dir("/abc")
+
+    if valid {
+        t.Errorf("Invalid directory is valid")
     }
 }
