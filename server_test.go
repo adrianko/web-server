a0f9c9d23909df95bdd5c6972d22db3668a4045c
Adrian Ko <adrianchko@gmail.com>
2015-07-24 11:53:39 +0100

    Add comments for some format bytes test

--- server_test.go
+++ server_test.go
@@ -53,7 +53,7 @@ func Test_get_extension__valid(t *testing.T) {
 }
 
 // Retrieve the file extension from "/bin"
-// SHould return empty string
+// Should return empty string
 func Test_get_extension__invalid(t *testing.T) {
     ext := get_extension("/bin")
 
@@ -62,6 +62,8 @@ func Test_get_extension__invalid(t *testing.T) {
     }
 }
 
+// Test whether 1023 bytes is formatted correctly
+// Should return "1023 B"
 func Test_format_bytes__bytes(t *testing.T) {
     bytes := 1023
     formatted := format_bytes(int64(bytes))
@@ -71,6 +73,8 @@ func Test_format_bytes__bytes(t *testing.T) {
     }
 }
 
+// Test whether 2048 bytes is formatted correctly
+// Should return "2.0 KB"
 func Test_format_bytes_kilobytes(t *testing.T) {
     bytes := 2048
     formatted := format_bytes(int64(bytes))
@@ -80,6 +84,8 @@ func Test_format_bytes_kilobytes(t *testing.T) {
     }
 }
 
+// Test whether 1048576 bytes is formatted correctly
+// Should return "1.0 MB"
 func Test_format_bytes_megabytes(t *testing.T) {
     bytes := 1048576
     formatted := format_bytes(int64(bytes))
@@ -89,6 +95,8 @@ func Test_format_bytes_megabytes(t *testing.T) {
     }
 }
 
+// Test whether 3221225472 bytes is formatted correctly
+// Should return "3.0 GB"
 func Test_format_bytes_gigabytes(t *testing.T) {
     bytes := 3221225472
     formatted := format_bytes(int64(bytes))
